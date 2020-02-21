5fe6b0c4542ce2d65bfc95e38fdef2fa096dd9dc
Naftaly Muiruri <muirurigathairu@gmail.com>
2020-02-21 14:52:09 -0500

    adds toggle method in custom js file

---
+++ js/delani.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    $(".img1").click(function(){
+        $(".hidden1").toggle()
+    });
+
+    $(".img2").click(function(){
+        $(".hidden2").toggle()
+    });
+    
+    $(".img3").click(function(){
+        $(".hidden3").toggle()
+    });
+
+});
