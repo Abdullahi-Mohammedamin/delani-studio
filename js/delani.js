1db5c4dbe30f5145263aeaa6f5b34e8e1a5fb662
Naftaly Muiruri <muirurigathairu@gmail.com>
2020-02-21 15:16:14 -0500

    adds portfolio section in html and its corresponding styles and images

--- js/delani.js
+++ js/delani.js
@@ -1,15 +1,18 @@
 $(function(){
 
-    $(".img1").click(function(){
-        $(".hidden1").slideToggle(1600)
+    $(".col-md-4").click(function(){
+        $(".hidden1").slideToggle(1600) 
+        $(".img1").slideToggle(1600)       
     });
 
-    $(".img2").click(function(){
+    $(".col-md-4").click(function(){
         $(".hidden2").slideToggle(1600)
+        $(".img2").slideToggle(1600)
     });
     
-    $(".img3").click(function(){
+    $(".col-md-4").click(function(){
         $(".hidden3").slideToggle(1600)
+        $(".img3").slideToggle(1600)
     });
 
 });
