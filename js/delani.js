eb2691679d15d9d32c629d6bd9e4e571aee7752e
Naftaly Muiruri <muirurigathairu@gmail.com>
2020-02-21 14:56:39 -0500

    changes toggle style to slide

--- js/delani.js
+++ js/delani.js
@@ -1,15 +1,15 @@
 $(function(){
 
     $(".img1").click(function(){
-        $(".hidden1").toggle()
+        $(".hidden1").slideToggle(1600)
     });
 
     $(".img2").click(function(){
-        $(".hidden2").toggle()
+        $(".hidden2").slideToggle(1600)
     });
     
     $(".img3").click(function(){
-        $(".hidden3").toggle()
+        $(".hidden3").slideToggle(1600)
     });
 
 });
