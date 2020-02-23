6c04a9ff7332a67476e219ccc2ae742598080804
Naftaly Muiruri <muirurigathairu@gmail.com>
2020-02-23 12:21:37 -0500

    adds div to portfolio icons to target them specifically

--- js/delani.js
+++ js/delani.js
@@ -1,16 +1,16 @@
 $(function(){
 
-    $(".col-md-4").click(function(){
+    $("#col-md-4-1").click(function(){
         $(".hidden1").slideToggle(1600) 
         $(".img1").slideToggle(1600)       
     });
 
-    $(".col-md-4").click(function(){
+    $("#col-md-4-2").click(function(){
         $(".hidden2").slideToggle(1600)
         $(".img2").slideToggle(1600)
     });
     
-    $(".col-md-4").click(function(){
+    $("#col-md-4-3").click(function(){
         $(".hidden3").slideToggle(1600)
         $(".img3").slideToggle(1600)
     });
